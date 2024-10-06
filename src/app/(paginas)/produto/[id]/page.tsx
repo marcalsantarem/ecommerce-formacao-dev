39cedee201668e737306d872c5df377c65f7d898
Marcal <marcalsantarem@gmail.com>
2024-10-05 23:38:34 -0300

    Ajustada página do produto para exibir ProdutoNaoEncontrado caso o produto acessado não exista

--- src/app/(paginas)/produto/[id]/page.tsx
+++ src/app/(paginas)/produto/[id]/page.tsx
@@ -1,11 +1,14 @@
+import ProdutoNaoEncontrado from "@/components/produto/ProdutoNaoEncontrado";
 import { produtos } from "@/core";
 
 export default function PaginaProduto(props: any) {
     const id = +props.params.id;
     const produto = produtos.find((produto) => produto.id === id);
-    return (
+    return produto ? (
         <div>
             <h1>Produto {id} - {produto?.nome}</h1>
         </div>
+    ) : (
+        <ProdutoNaoEncontrado />
     )
 }
